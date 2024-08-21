2638fc74470e2c81477a49fe3a9a431323f89893
Johan Riascos <desarrollador7777@gmail.com>
2024-08-21 09:42:26 -0500

    Implementation: Redux

--- App.js
+++ App.js
@@ -2,13 +2,16 @@ import {NavigationContainer} from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from "./screens/Home";
 import AddTodo from "./screens/AddTodo"
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
+
 const Stack = createNativeStackNavigator();
 
 
 
 export default function App() {
   return (
- 
+ <Provider store={store}>
        <NavigationContainer>
        <Stack.Navigator>
        <Stack.Screen
@@ -22,7 +25,7 @@ export default function App() {
        </Stack.Navigator>
        </NavigationContainer>
 
-
+       </Provider>
   );
 }
 
---
+++ redux/todosSlice.js
@@ -0,0 +1,47 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    todos: []
+};
+
+export const todosSlice = createSlice({
+    name: 'todos',
+    initialState: initialState,
+    reducers: {
+        setTodosReducer: (state, action) => {
+            state.todos = action.payload;
+            console.log(state.todos);
+        },
+        addTodoReducer: (state, action) => {
+            state.todos.push(action.payload);
+        },
+        hideComplitedReducer: (state) => {
+            state.todos = state.todos.filter(todo => !todo.isComplited);
+        },
+        updateTodoReducer: (state, action) => {
+            state.todos = state.todos.map(todo => {
+                if (todo.id === action.payload.id) {
+                    todo.isComplited = !todo.isComplited;
+                }
+                return todo;
+            });
+        },
+        deleteTodoReducer: (state, action) => {
+            const id = action.payload;
+            const todos = state.todos.filter(todo => todo.id !== id);
+            state.todos = todos;
+        }
+    }
+});
+
+// Exporta los reducers
+export const {
+    setTodosReducer,
+    addTodoReducer,
+    updateTodoReducer,
+    hideComplitedReducer,
+    deleteTodoReducer
+} = todosSlice.actions;
+
+// Exporta el reducer de todoSlice como default
+export default todosSlice.reducer;
